088cf251923a87cddf5d1c8522c78ae84132f769
patrycjamic <40001103+patrycjamic@users.noreply.github.com>
2020-07-25 07:46:51 +0200

    update

--- main.js
+++ main.js
@@ -2,21 +2,44 @@
 $( document ).ready(function() {
 
 
-    $(".nav-left").hover(function(){
+    // $(".nav-left").hover(function(){
+
+
+    //     $(".nav-left").toggle(function(){
+    //         $(".nav-container2").show("slow").css("width", "12vw").css("background-color", "white");
+
+
+    //         //   $(this).find('.nav-left').animate({
+    //         //         bottom: '0%'    
+    //         //         }, 'fast' );
+    //         //     },function() {
+    //         //     $(this).find('.nav-left').animate({
+    //         //         bottom: '-100%'
+    //         // },'fast');
         
-        // $(".nav-container2").show("slow").css("width", "12vw").css("background-color", "white");
+        
+    //         });
 
+    //     })
 
-      $(this).find('.nav-left').animate({
-            bottom: '0%'    
-            }, 'fast' );
-        },function() {
-        $(this).find('.nav-left').animate({
-            bottom: '-100%'
-    },'fast');
-
-
+    $(function(){
+        $(".nav-left").hover(function(){
+        $(this).parent().css("background-color", "white");
+        $(this).parent().animate({
+            width: "12vh",
+            height: "100vh"
+        });
+        // $(this).show("slow").css("width", "12vw").css("background-color", "white");
+        }, function(){
+            // change to any color that was previously used.
+            $(this).parent().css("width", "4vw");
+        });
     });
+
+
+    
+        
+
 
 });
 
