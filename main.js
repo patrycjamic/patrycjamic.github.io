aaf6d8217a0424d51522d3272d9c4df59b6dfb91
patrycjamic <40001103+patrycjamic@users.noreply.github.com>
2020-07-25 13:46:48 +0200

    update

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
 
 $( document ).ready(function() {
 
-
-    // $(".nav-left").hover(function(){
-
-
-    //     $(".nav-left").toggle(function(){
-    //         $(".nav-container2").show("slow").css("width", "12vw").css("background-color", "white");
-
-
-    //         //   $(this).find('.nav-left').animate({
-    //         //         bottom: '0%'    
-    //         //         }, 'fast' );
-    //         //     },function() {
-    //         //     $(this).find('.nav-left').animate({
-    //         //         bottom: '-100%'
-    //         // },'fast');
-        
-        
-    //         });
-
-    //     })
-
-    // $('.icon')
+    $(".menu-mobile").click(function(){
+        $(".lista2").slideToggle();
+    })
 
     $( ".icon" ).hover(
         function() {
@@ -42,7 +23,6 @@ $( document ).ready(function() {
         $(this).parent().css("background-color", "white");
         $(this).parent().animate({
             width: "18vw",
-            // height: "100vh"
         });
 
         $(".lista").animate({
@@ -54,26 +34,19 @@ $( document ).ready(function() {
         $(".menu").css("border-bottom", "double grey");
 
 
-        $('.logo').stop().slideUp('slow');
+        $('.slider').stop().slideUp('slow');
 
 
         }, function(){
-            // change to any color that was previously used.
-            $(this).parent().css("width", "4vw");
+            $(this).parent().animate({
+                width: "4vw",
+            });
             $(".lista").css("display", "none");
             $(".menu").css("border", "none");
 
-            $('.logo').stop().slideDown('slow');
+            $('.slider').stop().slideDown('slow');
 
-
-            // $(".nav-top").css("width", "16vw");
         });
     });
-
-
-    
-        
-
-
 });
 
